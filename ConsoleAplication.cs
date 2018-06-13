71fc07f4580ce10ffb72914c90a28db8c76968d2
vorwid <vorwid@gmail.com>
2018-06-13 22:29:56 +0200

    with continue

---
+++ ConsoleAplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0) continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
